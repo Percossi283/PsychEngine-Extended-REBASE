24726ba9f58b048a43c211f45231df7fa0ceea24
MaoPou <166735337+MaoPou@users.noreply.github.com>
2024-10-07 00:15:54 +0800

    Update SUtil.hx

--- source/mobile/SUtil.hx
+++ source/mobile/SUtil.hx
@@ -7,6 +7,8 @@ import android.content.Context as AndroidContext;
 import android.os.Environment as AndroidEnvironment;
 import android.Permissions as AndroidPermissions;
 import android.Settings as AndroidSettings;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
 #end
 #if sys
 import sys.FileSystem;
