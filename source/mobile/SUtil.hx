0457a9e57b1d32bd2c9ceb8e635eb864f06e4b2d
MaoPou <166735337+MaoPou@users.noreply.github.com>
2024-10-07 00:02:40 +0800

    Update SUtil.hx

--- source/mobile/SUtil.hx
+++ source/mobile/SUtil.hx
@@ -135,8 +135,8 @@ class SUtil
 		if (!AndroidPermissions.getGrantedPermissions().contains('android.permission.READ_EXTERNAL_STORAGE')
 			&& !AndroidPermissions.getGrantedPermissions().contains('android.permission.WRITE_EXTERNAL_STORAGE'))
 		{
-			AndroidPermissions.requestPermission('READ_EXTERNAL_STORAGE');
-			AndroidPermissions.requestPermission('WRITE_EXTERNAL_STORAGE');
+			AndroidPermissions.requestPermissions('READ_EXTERNAL_STORAGE');
+			AndroidPermissions.requestPermissions('WRITE_EXTERNAL_STORAGE');
 			showPopUp('If you accepted the permissions you are all good!' + '\nIf you didn\'t then expect a crash' + '\nPress Ok to see what happens',
 				'Notice!');
 			if (!AndroidEnvironment.isExternalStorageManager())
